06fb906a35307b498944729f5becc79f22ece53e
Mario <31197372+Max131@users.noreply.github.com>
2024-01-08 15:51:13 -0600

    fix refactore

--- src/lib/DataTable.svelte
+++ src/lib/DataTable.svelte
@@ -1,44 +1,50 @@
 <script>
+  import SearchBar from "./SearchBar.svelte";
   export let data = [];
   export let itemsPerPage = 10;
 
   itemsPerPage ||= data.length;
 
+  let results;
   let currentPage = 0;
-  let paginatedData = data;
-
-  const PAGES = Math.ceil(data.length / itemsPerPage);
 
   const handlePagination = ({ currentTarget }) => {
     const { page } = currentTarget.dataset;
     currentPage = +page;
   };
 
-  $: paginatedData = data.slice(
+  $: paginatedData = results?.slice(
     currentPage * itemsPerPage,
     currentPage * itemsPerPage + itemsPerPage,
   );
+
+  $: PAGES = Math.ceil(results?.length / itemsPerPage);
 </script>
 
+<SearchBar {data} fields={["first_name", "email"]} bind:results />
 <table class="striped">
   <slot name="head" />
   <tbody>
-    {#each paginatedData as item}
-      <slot {item} />
-    {/each}
+    {#if paginatedData}
+      {#each paginatedData as item}
+        <slot {item} />
+      {/each}
+    {/if}
   </tbody>
 </table>
 
 {#if itemsPerPage !== data.length}
   <figure>
     <div role="group">
-      {#each Array(PAGES) as _, index}
-        <button
-          class={currentPage !== index ? "" : "outline secondary"}
-          data-page={index}
-          on:click={handlePagination}>{index + 1}</button
-        >
-      {/each}
+      {#if PAGES}
+        {#each Array(PAGES) as _, index}
+          <button
+            class={currentPage !== index ? "" : "outline secondary"}
+            data-page={index}
+            on:click={handlePagination}>{index + 1}</button
+          >
+        {/each}
+      {/if}
     </div>
   </figure>
 {/if}
